11d4d9233b2786b76a9eb4b9214257d1c3414fdf
unknown <jonathanscanio@gmail.com>
2019-06-03 21:50:15 -0300

    Number validation project

---
+++ ValidacionNumeroEntero/ValidacionNumeroEntero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionNumeroEntero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberFromUserInput="";
+            int numberAsInt;
+            bool isInt;
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero");
+                numberFromUserInput = Console.ReadLine();
+                isInt = int.TryParse(numberFromUserInput, out numberAsInt);
+                if (!isInt)
+                {
+                    Console.WriteLine("Su numero no es entero");
+                }
+            }
+            while (!isInt);
+
+        }
+    }
+}
