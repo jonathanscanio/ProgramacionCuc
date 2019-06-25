16f4bb3e9944df42a824647ce5673de4cc024df3
Fer <fpotichkin@gmail.com>
2019-06-25 17:35:29 -0300

    Ejercicio triangulo Fer

---
+++ Ahorcado/Ahorcado/Clasificacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+   public class triangulo
+    {
+        public int altura = 0;
+        public int baseT = 0;
+        public int sup;
+        
+        public void
+  CalculoSup()
+        {
+            sup = (baseT * altura)/2;
+            Console.WriteLine(sup);
+        }
+    }
+}
---
+++ Ahorcado/Ahorcado/articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+    class articulo
+    {
+        public int stock;
+        public float precio;
+        public float CalculoDescuento()
+        {
+            if (stock < 3)
+                precio = precio - (precio * 20) / 100;
+            else
+                precio = precio - (precio * 30) / 100;
+            return precio;
+        }
+    }
+}
