a67add8f4727993c10c1bd409f85bc62f698fab2
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2019-07-21 00:20:25 -0300

    ahorcado

---
+++ Ahorcado/Ahorcado/Partida.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ahorcado
+{
+    class Partida
+    {
+        public string playerName;
+        public string palabraElegida;
+        public StringBuilder sbGuiones; 
+        public bool win = false; 
+
+        public void GetPlayerName(int ignore) 
+        {
+            do
+            {
+                Console.Write("Dame el nombre del jugador " + ignore + ": ");
+                playerName = Console.ReadLine();
+                if (playerName == "")
+                {
+                    Console.WriteLine("El jugador debe tener nombre.");
+                }
+                else break;
+            } while (true);
+        }
+        public void GetPalabraElegida() 
+        {
+            bool esPalabra;
+            do
+            {
+                Console.Write("Escriba la palabra para el jugador " + playerName + ": ");
+                palabraElegida = "";
+
+                do //algoritmo de internet, escribe la palabra como contraseña, no tocar.
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    // Backspace Should Not Work
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        palabraElegida += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && palabraElegida.Length > 0)
+                        {
+                            palabraElegida = palabraElegida.Substring(0, (palabraElegida.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                        else if (key.Key == ConsoleKey.Enter)
+                        {
+                            break;
+                        }
+                    }
+                } while (true);
+                Console.WriteLine("");
+
+                if (palabraElegida.Length > 1)
+                {
+                    palabraElegida = palabraElegida.ToUpper();
+                    if (palabraElegida.Contains("\0")) //manejo de tildes, no tocar, se complementa con el algoritmo anterior.
+                    {
+                        palabraElegida = palabraElegida.Remove(palabraElegida.IndexOf("\0"), 1);
+                        palabraElegida = palabraElegida.Replace("Á", "A");
+                        palabraElegida = palabraElegida.Replace("É", "E");
+                        palabraElegida = palabraElegida.Replace("Í", "I");
+                        palabraElegida = palabraElegida.Replace("Ó", "O");
+                        palabraElegida = palabraElegida.Replace("Ú", "U");
+                    }
+                    esPalabra = Regex.IsMatch(palabraElegida, @"^[A-Z]+$");
+                }
+                else esPalabra = false;
+                
+                if (!esPalabra)
+                {
+                    Console.WriteLine("La palabra elegida no es válida.");
+                }
+            } while (!esPalabra);
+        }
+        public void IniciarGuiones()
+        {
+            sbGuiones = new StringBuilder(palabraElegida);
+            for (int i = 0; i < palabraElegida.Length; i++)
+            {
+                sbGuiones[i] = '_';
+            }
+        }
+        public void ImprimirGuiones()
+        {
+            Console.Clear();
+            Console.WriteLine("Ahorcado correspondiente a: " + playerName + "\n\n\n\n");
+            Console.Write("            ");
+            for (int i = 0; i < palabraElegida.Length; i++)
+            {
+                Console.Write(" " + sbGuiones[i].ToString());
+            }
+            Console.WriteLine("");
+        }
+        public bool LogicaReemplazar(string intento)
+        {
+            bool reemplazo = false;
+            if (intento.Length == 1)
+            {
+                if (palabraElegida.Contains(intento))
+                {
+                    for (int i = 0; i < palabraElegida.Length; i++)
+                    {
+                        if (intento == palabraElegida[i].ToString())
+                        {
+                            sbGuiones[i] = intento[0]; //No se que hace el [0] pero funciona, no tocar.
+                            reemplazo = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (intento == palabraElegida)
+                {
+                    sbGuiones = new StringBuilder(palabraElegida);
+                    reemplazo = true;
+                }
+            }
+            if (!sbGuiones.ToString().Contains("_"))
+            {
+                win = true;
+            }
+            return reemplazo;
+        }
+    }
+}
--- Ahorcado/Ahorcado/Program.cs
+++ Ahorcado/Ahorcado/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,178 @@ namespace Ahorcado
     {
         static void Main(string[] args)
         {
+            /* 
+            Programa ahorcado, observaciones:
+            El programa funciona, creo que realize todas las comprobaciones necesarias para que no se pueda trabar.
+            Me base en un modo de juego multijugador donde no hay vidas y gana el primero que adivine la palabra.
+            Por lo tanto si juega una sola persona (cosa que es posible), no tendria sentido porque nunca perderia.
+            Se podria programar para que si juega una sola persona se active el sistema de vidas pero no lo hice.
+            Edit: Si el jugador acierta el intento, continua jugando, se pasa al siguiente jugador cuando falla el intento
+            Pueden ganar varias personas a la vez, o sea si la persona que comenzo jugando gana, los demas todavia tienen una
+            ultima chance de completar la palabra debido a que empezaron despues. Todos tienen la misma cantidad de intentos
+            
+            Respecto a las comprobaciones:
+                -Cantidad de jugadores:
+                        .Desde 1 hasta 20.
+                -El nombre del jugador: 
+                        .Tiene que tener al menos un caracter
+                        .Puede ser infinitos caracteres siempre y cuando no esten repetidos con un jugador previo.
+                -La palabra elegida:
+                        .Desde 2 letras, no hay limite, puede ser infinitas
+                        .No puede tener espacios
+                        .Puede ser solamente letras, en caso de que escriba una palabra con tilde en una vocal el programa automaticamente 
+                        lo elimina y toma la palabra como valida. Si hay dos tildes en vocales distintas la palabra no sera valida.
+                -El intento:
+                        .Si el jugador en presiona enter para ingresar el intento, se va a tomar como que no apreto nada.
+                        .Se reemplazan las vocales con tilde por vocales normales, si es que tiene.
+                        .Si es una palabra (mas de dos caracteres) en vez de un solo caracter, se toma como que arriesgo adivinar la palabra,
+                        al no haber vidas, si arriegas y no acertas, se cuenta como un intento fallido.
+
+            Las palabras y los intentos se pasan a mayusculas
+
+            Posibles cambios:
+            -Permitir que las palabra elegidas puedan ser frases
+            -Configurar modo 1 jugador, con vidas
+            -Mostrar los intentos fallidos que lleva cada jugador
+            -Mostrar la palabra correcta a los que perdieron
+
+            Comunicacion con los usuarios:
+            Puse mensajes muy breves y no se explican las reglas o como funciona el programa,
+            Se puede mejorar bastante este aspecto, sobre todo al mostrar los ganadores.
+
+            Dejo mensajes para que sea mas facil comprender la logica
+            */
+
+
+            Console.WriteLine("Juego ahorcado\n");
+
+            // PEDIR CANTIDAD DE JUGADORES
+
+            //variables necesarias 
+            int players;
+            bool isInt;
+
+            do
+            {
+                Console.Write("¿Cuántas personas van a jugar?: ");
+                isInt = int.TryParse(Console.ReadLine(), out players);
+                if (!isInt)
+                {
+                    Console.WriteLine("La cantidad de jugadores no es válida.");
+                }
+                else if (players <= 0 || players > 20)
+                {
+                    Console.WriteLine("La cantidad de jugadores no es válida.");
+                    isInt = false;
+                }
+            }
+            while (!isInt);
+
+            Console.Clear();
+
+            // PEDIR NOMBRE DE CADA JUGADOR Y SU PALABRA. (TAMBIEN APROVECHO EL CICLO PARA INICIAR EL SB GUIONES)
+
+            //variables necesarias 
+            Partida[] partida = new Partida[players];
+            Queue playersQueue = new Queue();
+
+            for (int i = 0; i < players; i++)
+            {
+                partida[i] = new Partida();
+                do
+                {
+                    partida[i].GetPlayerName(i + 1); //ignorar el i+1, es para que se vea mejor en la consola
+                    if (playersQueue.Contains(partida[i].playerName))
+                    {
+                        Console.WriteLine("El jugador " + partida[i].playerName + " ya existe.");
+                    }
+                    else
+                    {
+                        playersQueue.Enqueue(partida[i].playerName);
+                        break;
+                    }
+                } while (true);
+                partida[i].GetPalabraElegida();
+                partida[i].IniciarGuiones();
+                Console.Clear();
+            }
+
+            // LOGICA PRINCIPAL DEL PROGRAMA
+
+            //variables necesarias 
+            Queue winners = new Queue();
+            string intento;
+            bool reemplazo;
+
+            do //ciclo principal del juego, termina cuando hay ganadores
+            {
+                for (int i = 0; i < players; i++) //ciclo para que pasen todos los jugadores, termina cuando cada jugador pasa por el ciclo
+                {
+                    do //ciclo que pide y comprueba intentos, imprime el ahorcado, termina cuando falla o gana
+                    {
+                        partida[i].ImprimirGuiones();
+
+                        Console.WriteLine("\n"); //ignorar
+
+                        do //ciclo que pide intentos
+                        {
+                            Console.Write("Dame el intento: ");
+                            intento = Console.ReadLine().ToUpper();
+                            if (intento == "") //if para ignorar si el jugador no ingresa nada
+                            {
+                                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                                Console.Write(new string(' ', Console.WindowWidth));
+                                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                            }
+                            else 
+                            {
+                                intento = intento.Replace("Á", "A");
+                                intento = intento.Replace("É", "E");
+                                intento = intento.Replace("Í", "I");
+                                intento = intento.Replace("Ó", "O");
+                                intento = intento.Replace("Ú", "U");
+                                break;
+                            }
+                        } while (true);
+
+                        reemplazo = partida[i].LogicaReemplazar(intento);
+
+                        if (reemplazo)// if para optimizar, ignorar
+                        {
+                            partida[i].ImprimirGuiones();
+                        }
+
+                    } while (reemplazo && !partida[i].win);
+
+                    if (!partida[i].win)
+                    {
+                        Console.WriteLine("\n\nIntento fallido.\nPresiona cualquier tecla para pasar al siguiente jugador: ");
+                    }
+                    else
+                    {
+                        winners.Enqueue(partida[i].playerName);
+                        Console.WriteLine("\n\nPalabra completada!!!\nPresiona cualquier tecla para pasar al siguiente jugador: ");
+                    }
+                    Console.ReadKey(true);
+                }
+
+            } while (winners.Count == 0);
+
+            Console.Clear();
+
+
+            // MOSTRAR GANADORES
+
+            if (winners.Count == 1)
+            {
+                Console.WriteLine("El ganador es: \n");
+            }
+            else Console.WriteLine("Los ganadores son: \n");
+
+            foreach (string i in winners)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
