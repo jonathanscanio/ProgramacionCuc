20cab787fe981441a64786dc6fd7caa92210796e
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2019-09-19 19:25:37 -0300

    Añadidos: Modelo Partida

---
+++ AhorcadoWindowsForms/AhorcadoWindowsForms/src/Modelo/Partida.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoWindowsForms.src.Modelo
+{
+    class Partida
+    {
+        string palabra;
+        int vidas;
+        StringBuilder guiones;
+        string intentosErrones;
+        string pista;
+
+        #region Controles Publicos (get, set)
+
+        public string Palabra { get => palabra; set => palabra = value; }
+        public int Vidas { get => vidas; set => vidas = value; }
+        public StringBuilder Guiones { get => guiones; set => guiones = value; }
+        public string IntentosErroneos { get => intentosErrones; set => intentosErrones = value; }
+        public string Pista { get => pista; set => pista = value; }
+
+        #endregion
+
+    }
+}
